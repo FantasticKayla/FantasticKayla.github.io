715546114f5e9fffd4d9a43d0b1f362da05aac7a
Kayla <kaylalee0000@gmail.com>
2017-07-15 20:14:05 +0000

    starting collaborative sketch

---
+++ collaborative_sketch/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2vBjZZYIY4iAzF1MbYrPC6JqQwgizDDA",
+    authDomain: "collaborative-sketch-ce756.firebaseapp.com",
+    databaseURL: "https://collaborative-sketch-ce756.firebaseio.com",
+    projectId: "collaborative-sketch-ce756",
+    storageBucket: "collaborative-sketch-ce756.appspot.com",
+    messagingSenderId: "1012650836262"
+  };
+  firebase.initializeApp(config);
